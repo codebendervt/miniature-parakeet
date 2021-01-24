6d68e41713c1f3b068bdbf88c96f4bd042e4e2b3
codebender <47385943+rawkthethinker@users.noreply.github.com>
2021-01-24 17:45:39 +0200

    I cant use the key is preview it looks like it

--- financial/paystack/utils/init.js
+++ financial/paystack/utils/init.js
@@ -3,7 +3,7 @@ import transact from 'sauveur_core/fetch/fetch.js'
 
 const endpoint = "https://api.paystack.co/"
 
-let key = process.env.PAYSTACK_SECRET;
+let key = process.env.PAYSTACK_SECRET || "pk_test_8dc7324a031b0de76d7ae9f359fb1ae91fac3712";
 
 const payStackAPi =  async (method, type, data = null) => {
 
